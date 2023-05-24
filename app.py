fdcdf398a67973bdd3e8a6bdbc4e4c7c999a18a9
Bandi Avinash Bhargav <131337817+AvinashBhargav@users.noreply.github.com>
2023-05-24 23:19:48 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+#AB17
+from time import strftime
+from tkinter import Label, Tk
+ 
+window = Tk()
+window.title("")
+window.geometry("220x100")
+window.configure(bg="blue")  #clock BG
+window.resizable(False, False)  
+
+clock_label = Label(
+    window, bg="black", fg="red", font=("Arial", 30, "bold"), relief="flat"
+)
+clock_label.place(x=20, y=20)
+
+def update_label():
+
+    current_time = strftime("%H: %M: %S\n %d-%m-%Y ")
+    clock_label.configure(text=current_time)
+    clock_label.after(80, update_label)
+    clock_label.pack(anchor="center")
+
+update_label()
+window.mainloop()
