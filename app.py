0997c126bdcb438f24ae934597991331776e6efb
Bandi Avinash Bhargav <131337817+AvinashBhargav@users.noreply.github.com>
2023-05-26 22:53:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 #AB17
+#Import
 from time import strftime
 from tkinter import Label, Tk
- 
+#windowspecification
 window = Tk()
 window.title("")
 window.geometry("220x100")
 window.configure(bg="blue")  #clock BG
 window.resizable(False, False)  
-
+#clockspecification
 clock_label = Label(
     window, bg="black", fg="red", font=("Arial", 30, "bold"), relief="flat"
 )
 clock_label.place(x=20, y=20)
-
+#def and update
 def update_label():
 
     current_time = strftime("%H: %M: %S\n %d-%m-%Y ")
